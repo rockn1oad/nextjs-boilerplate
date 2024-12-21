94de4bc62f994f97363f1e9c2967c1b2c377e4a0
Denis Babineau <denis.babineau@moncton.ca>
2024-12-21 15:03:51 -0400

    fix bug

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
 
-        <h1 className="text-3xl font-bold uppercase">Don's Music<br />Boutique</h1>
+        <h1 className="text-3xl font-bold uppercase">Don&apos;s Music<br />Boutique</h1>
 
 
         <Tabs defaultValue="artist" className="w-[1200px]">
