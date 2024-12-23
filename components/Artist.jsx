ff7146276a8bd701aedebf85429c30e0c271304e
Denis Babineau <denis.babineau@moncton.ca>
2024-12-23 15:04:13 -0400

    more updates

--- components/Artist.jsx
+++ components/Artist.jsx
@@ -1,18 +1,10 @@
 import classes from './Artist.module.css'
-import Image from 'next/image'
 
 function Artist(props ) {
   return (
     <div className={classes.card}>
 
-      {/* <Image
-        src="/artists/kill-chicago.jpg"
-        width={500}
-        height={500}
-        alt="{props.name}"
-      /> */}
-
-      <img src={props.img} />
+      <img src={props.img} alt="{props.name}"/>
 
       <h2>{props.name}</h2>
 
--- components/Artists.jsx
+++ components/Artists.jsx
@@ -6,38 +6,29 @@ import Artist from "@/components/Artist";
 import NewArtist from "@/components/NewArtist";
 import Modal from "@/components/Modal";
 
-
 function Artists() {
-  const [enteredBody, setEnteredbody] = useState('');
-  const [enteredArtist, setEnteredArtist] = useState('');
 
-  function bodyChangeHandler(event) {
-    setEnteredbody(event.target.value);
-  }
-
-  function ArtistChangeHandler(event) {
-    setEnteredArtist(event.target.value);
-  }
 
   return (
-    <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-4">
+    <>
       {/* <Modal>
-        <NewArtist 
-          onArtistChange={ArtistChangeHandler} 
-          onBodyChange={bodyChangeHandler} 
+        <NewArtist
+          onArtistChange={ArtistChangeHandler}
+          onBodyChange={bodyChangeHandler}
         />
       </Modal> */}
-      {/* <Artist name={enteredBody} body={enteredArtist} img="before-the-dinosaurs.jpg" /> */}
-      <Artist name="Before The Dinosaurs" img="artists/before-the-dinosaurs.jpg" />
-      <Artist name="Kill Chicago" img="artists/kill-chicago.jpg" />
-      <Artist name="North of Nowhere" img="artists/north-of-nowhere.jpeg" />
-      <Artist name="Melonvine" img="artists/melonvine.jpg" />
-      <Artist name="Motorleague" img="artists/the-motorleague.jpg" />
-      <Artist name="Run the river" img="artists/run-the-river.jpg" />
-      <Artist name="Secondsdown" img="artists/seconsdown.jpg" />
-      <Artist name="Sig Sour" img="sig-sour.jpg" />
-      <Artist name="Too Bad Jim" img="too-bad-jim.jpg" />
-    </div>
+      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-4">
+        <Artist name="Before The Dinosaurs" img="artists/before-the-dinosaurs.jpg" />
+        <Artist name="Kill Chicago" img="artists/kill-chicago.jpg" />
+        <Artist name="North of Nowhere" img="artists/north-of-nowhere.jpeg" />
+        <Artist name="Melonvine" img="artists/melonvine.jpg" />
+        <Artist name="Motorleague" img="artists/the-motorleague.jpg" />
+        <Artist name="Run the river" img="artists/run-the-river.jpg" />
+        <Artist name="Secondsdown" img="artists/seconsdown.jpg" />
+        <Artist name="SigSour" img="artists/sigsour.jpg" />
+        <Artist name="Too Bad Jim" img="artists/too-bad-jim.jpg" />
+      </div>
+    </>
   );
 }
 
